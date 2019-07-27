2ed4bdab4559f95398ffae52252cbec08fb00e6f
jkingz <jfkrule@gmail.com>
2019-07-28 00:37:51 +0800

    Updated react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+import Form from './components/Form';
+import People from './components/People';
+import NewPerson from './components/NewPerson';
+
 const App = () => {
   const [people, setPeople] = useState([
     {
@@ -11,69 +15,17 @@ const App = () => {
       lastName: 'jkingz',
     },
   ]);
-  const [person, setPerson] = useState({ firstName: '', lastName: '' });
-  console.log(person, 'person', setPerson, 'setPerson');
-  const onChange = e => {
-    console.log(e);
-    setPerson({ ...person, [e.target.name]: e.target.value });
-  };
-  const onSubmit = e => {
-    console.log(e, 'sdasd');
-    e.preventDefault();
-    if (person.firstName.trim() === '' || person.lastName.trim() === '') return;
 
-    const newPerson = {
-      firstName: person.firstName.trim(),
-      lastName: person.lastName.trim(),
-    };
-
-    setPeople([...people, newPerson]);
-    person.firstName = '';
-    person.lastName = '';
+  const addPerson = person => {
+    setPeople([...people, person]);
   };
+
   return (
-    <div className='container'>
+    <div className='container mt-4'>
       <div className='row'>
-        <div className='col'>
-          <h2>Add a person</h2>
-          <hr />
-          <form onSubmit={onSubmit}>
-            <div className='form-group'>
-              <input
-                type='text'
-                className='form-control'
-                name='firstName'
-                placeholder='First name.'
-                value={person.firstName}
-                onChange={onChange}
-              />
-            </div>
-            <div className='form-group'>
-              <input
-                type='text'
-                className='form-control'
-                name='lastName'
-                placeholder='Last name.'
-                value={person.lastName}
-                onChange={onChange}
-              />
-            </div>
-            <button className='btn btn-success' type='submit'>
-              Add person
-            </button>
-          </form>
-        </div>
-        <div className='col'>
-          <h2>People:</h2>
-          <hr />
-          {people.map(p => (
-            <div key={Math.random() * 1000000000}>
-              <p>
-                {p.firstName} {p.lastName}
-              </p>
-            </div>
-          ))}
-        </div>
+        <Form addPerson={addPerson} />
+        <People people={people} />
+        <NewPerson newPerson={people[people.length - 1]} />
       </div>
     </div>
   );
